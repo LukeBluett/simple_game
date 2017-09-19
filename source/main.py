4523ce1d84624a4718ad50b045d192fc3110f494
Luke Bluett <Luke.J.Bluett@gmail.com>
2017-09-19 15:51:08 +0100

    Made more readable
    modified:   source/main.py
    Removed some variables that were not needed, and used the variables
    display_width and display_height to make code more readable.
    
    Added code for checking if alien was not passed the display width and
    height.

--- source/main.py
+++ source/main.py
@@ -19,6 +19,21 @@ def draw():
     game_display.fill(black)
     my_alien.draw(game_display)
 
+def set_boundaries():
+    if my_alien.get_x_position() > (display_width - 80):
+        my_alien.set_x_position(display_width - 80)
+
+    if my_alien.get_x_position() < 0:
+        my_alien.set_x_position(0)
+
+    if my_alien.get_y_position() > (display_height - 50):
+        my_alien.set_y_position(display_height - 50)
+
+    if my_alien.get_y_position() < 0:
+        my_alien.set_y_position(0)
+    
+
+
 def events(my_alien):
     if event.type == pygame.QUIT:
         pygame.quit()
@@ -64,22 +79,7 @@ clock = pygame.time.Clock()
 my_alien = Alien(100, 100, 'resource/roughspaceship.png')
 
 while game_is_running:
-    
-    x = my_alien.get_x_position()
-    y = my_alien.get_y_position()
- 
-    if x > (600 - 80):
-        my_alien.set_x_position(600 - 80)
-
-    if x < 0:
-        my_alien.set_x_position(0)
-
-    if y > (400 - 50):
-        my_alien.set_y_position(400 - 50)
-
-    if y < 0:
-        my_alien.set_y_position(0)
-    
+    set_boundaries()
     draw()
     for event in pygame.event.get():
         events(my_alien)
